53e80561a77e9a18da6934216ed78d9b954d011e
Matt Furden <zolrath@gmail.com>
2023-01-10 15:38:58 -0800

    support wsl/darwin/linux clipboards

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -4,19 +4,27 @@
 --
 vim.opt.guifont = "JetBrainsMono Nerd Font:h11"
 
-local in_wsl = os.getenv("WSL_DISTRO_NAME") ~= nil
+-- Set paste directly - greatly improves startup time
+local is_wsl = os.getenv("WSL_DISTRO_NAME") ~= nil
+local is_osx = vim.loop.os_uname().sysname == "Darwin"
 
-if in_wsl then
+if is_wsl then
   vim.g.clipboard = {
     copy = { ["+"] = { "clip.exe" }, ["*"] = { "clip.exe" } },
     paste = { ["+"] = { "nvim_paste" }, ["*"] = { "nvim_paste" } },
     cache_enabled = true,
   }
+elseif is_osx then
+  vim.g.clipboard = {
+    copy = { ["+"] = "pbcopy", ["*"] = "pbcopy" },
+    paste = { ["+"] = "pbpaste", ["*"] = "pbpaste" },
+    cache_enabled = true,
+  }
 else
   vim.g.clipboard = {
-    name = "xsel",
     copy = { ["+"] = "xsel --nodetach -i -b", ["*"] = "xsel --nodetach -i -p" },
     paste = { ["+"] = "xsel  -o -b", ["*"] = "xsel  -o -b" },
     cache_enabled = true,
   }
 end
+
