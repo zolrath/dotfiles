af39a4216dbf4680f6f8be50bfa814acb75d5b94
Matt Furden <zolrath@gmail.com>
2023-01-10 15:26:51 -0800

    explicitly define clipboards to increase startup time

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -3,3 +3,20 @@
 -- Add any additional options here
 --
 vim.opt.guifont = "JetBrainsMono Nerd Font:h11"
+
+local in_wsl = os.getenv("WSL_DISTRO_NAME") ~= nil
+
+if in_wsl then
+  vim.g.clipboard = {
+    copy = { ["+"] = { "clip.exe" }, ["*"] = { "clip.exe" } },
+    paste = { ["+"] = { "nvim_paste" }, ["*"] = { "nvim_paste" } },
+    cache_enabled = true,
+  }
+else
+  vim.g.clipboard = {
+    name = "xsel",
+    copy = { ["+"] = "xsel --nodetach -i -b", ["*"] = "xsel --nodetach -i -p" },
+    paste = { ["+"] = "xsel  -o -b", ["*"] = "xsel  -o -b" },
+    cache_enabled = true,
+  }
+end
