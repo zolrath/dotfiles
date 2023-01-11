62db147dd3185083e91f91c7a1e5b71dc4f0aec4
Matt Furden <zolrath@gmail.com>
2023-01-10 18:37:24 -0800

    use mix.format on elixir save

--- dot_config/nvim/lua/langs/elixir.lua
+++ dot_config/nvim/lua/langs/elixir.lua
@@ -31,4 +31,20 @@ return {
       },
     },
   },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local null_ls = require("null-ls")
+
+      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+      local formatting = null_ls.builtins.formatting
+      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+      local diagnostics = null_ls.builtins.diagnostics
+
+      opts.sources = opts.vim.list_extend(opts.sources, {
+        formatting.mix,
+        diagnostics.credo,
+      })
+    end,
+  },
 }
