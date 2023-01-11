914558b90c820cf95f0c79cfa57dd24c0e95e305
Matt Furden <zolrath@gmail.com>
2023-01-10 16:15:44 -0800

    disable copilot for now - improve cmp window size

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -1,24 +1,3 @@
-local has_copilot, copilot_cmp = pcall(require, "copilot_cmp.comparators")
-
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-    return false
-  end
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-end
-
-local types = require("cmp.types")
-
-local function deprioritize_snippet(entry1, entry2)
-  if entry1:get_kind() == types.lsp.CompletionItemKind.Snippet then
-    return false
-  end
-  if entry2:get_kind() == types.lsp.CompletionItemKind.Snippet then
-    return true
-  end
-end
-
 return {
   -- auto completion
   {
@@ -29,7 +8,6 @@ return {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "saadparwaiz1/cmp_luasnip",
-      "zbirenbaum/copilot-cmp",
     },
     opts = function()
       local cmp = require("cmp")
@@ -46,15 +24,9 @@ return {
           completion = {
             border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
             scrollbar = "║",
-            winhighlight = "Normal:CmpMenu,FloatBorder:CmpMenuBorder,CursorLine:CmpSelection,Search:None",
-            autocomplete = {
-              require("cmp.types").cmp.TriggerEvent.InsertEnter,
-              require("cmp.types").cmp.TriggerEvent.TextChanged,
-            },
           },
           documentation = {
             border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-            winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
             scrollbar = "║",
           },
         },
@@ -64,31 +36,18 @@ return {
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<C-c>"] = cmp.mapping.complete({
-            config = {
-              sources = {
-                { name = "copilot" },
-              },
-            },
-          }),
         }),
         sources = cmp.config.sources({
-          { name = "copilot" },
           { name = "nvim_lsp" },
           { name = "luasnip" },
           { name = "buffer" },
           { name = "path" },
         }),
         sorting = {
-          --keep priority weight at 2 for much closer matches to appear above copilot
-          --set to 1 to make copilot always appear on top
-          priority_weight = 2,
           comparators = {
             -- order matters here
             cmp.config.compare.exact,
             --deprioritize_snippet,
-            has_copilot and copilot_cmp.prioritize or nil,
-            has_copilot and copilot_cmp.score or nil,
             cmp.config.compare.offset,
             -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
             cmp.config.compare.score,
@@ -98,22 +57,43 @@ return {
             cmp.config.compare.sort_text,
             cmp.config.compare.length,
             cmp.config.compare.order,
-            -- personal settings:
-            -- cmp.config.compare.recently_used,
-            -- cmp.config.compare.offset,
-            -- cmp.config.compare.score,
-            -- cmp.config.compare.sort_text,
-            -- cmp.config.compare.length,
-            -- cmp.config.compare.order,
           },
         },
         preselect = cmp.PreselectMode.Item,
         formatting = {
           format = function(_, item)
+            -- set icons
             local icons = require("lazyvim.config").icons.kinds
             if icons[item.kind] then
               item.kind = icons[item.kind] .. item.kind
             end
+
+            -- set width to a constant size
+            local ELLIPSIS_CHAR = "…"
+            local MAX_LABEL_WIDTH = 20
+            local MIN_LABEL_WIDTH = 20
+
+            local label = item.abbr
+            local truncated_label = vim.fn.strcharpart(label, 0, MAX_LABEL_WIDTH)
+            if truncated_label ~= label then
+              item.abbr = truncated_label .. ELLIPSIS_CHAR
+            elseif string.len(label) < MIN_LABEL_WIDTH then
+              local padding = string.rep(" ", MIN_LABEL_WIDTH - string.len(label))
+              item.abbr = label .. padding
+            end
+
+            local MAX_MENU_WIDTH = 20
+            local MIN_MENU_WIDTH = 20
+
+            local menu = item.menu
+            local truncated_menu = vim.fn.strcharpart(menu, 0, MAX_MENU_WIDTH)
+            if truncated_menu ~= menu then
+              item.menu = truncated_menu .. ELLIPSIS_CHAR
+            elseif string.len(menu) < MIN_MENU_WIDTH then
+              local padding = string.rep(" ", MIN_MENU_WIDTH - string.len(menu))
+              item.menu = menu .. padding
+            end
+
             return item
           end,
         },
--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,11 +1,13 @@
 return {
   {
     "zbirenbaum/copilot-cmp",
+    enabled = false,
     opts = {},
   },
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
+    enabled = false,
     lazy = false,
     opts = {
       suggestion = { enabled = false },
