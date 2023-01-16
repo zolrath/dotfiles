a6189f003f63d653efd467cad0f418ddfddaf7b9
Matt Furden <zolrath@gmail.com>
2023-01-16 15:31:11 -0800

    update copilot hotkey

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -21,7 +21,7 @@ return {
           auto_trigger = true,
           debounce = 75,
           keymap = {
-            accept = "<M-l>",
+            accept = "<M-;>",
             next = "<M-]>",
             prev = "<M-[>",
             dismiss = "<C-]>",
