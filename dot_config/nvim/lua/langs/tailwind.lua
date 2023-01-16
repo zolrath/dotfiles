20d3f51a13597c745b0084c11d0904e79ca80f53
Matt Furden <zolrath@gmail.com>
2023-01-16 12:54:26 -0800

    add highlight colors to tailwind in vim

--- dot_config/nvim/lua/langs/tailwind.lua
+++ dot_config/nvim/lua/langs/tailwind.lua
@@ -95,5 +95,13 @@ return {
         },
       },
     },
+    {
+      "brenoprata10/nvim-highlight-colors",
+      opts = {
+        render = "background", -- or 'foreground' or 'first_column'
+        enable_named_colors = true,
+        enable_tailwind = false,
+      },
+    },
   },
 }
