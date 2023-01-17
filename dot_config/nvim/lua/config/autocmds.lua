71c7fde72dc17b3c0a907ef067de5809e9056ad3
Matt Furden <zolrath@gmail.com>
2023-01-16 23:04:57 -0800

    allow closing terminals in vim with q

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,5 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+--
+--
+-- allow closing finished terminal windows with q
+--
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function(event)
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
 
 -- close some filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
