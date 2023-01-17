71131f0e7e5e326dc3266acb14e86463a9c8141d
Matt Furden <zolrath@gmail.com>
2023-01-17 13:06:50 -0800

    load friendly snippets and custom snippets dir

--- dot_config/nvim/lua/plugins/luasnip.lua
+++ dot_config/nvim/lua/plugins/luasnip.lua
@@ -6,6 +6,7 @@ return {
     dependencies = {
       "rafamadriz/friendly-snippets",
       config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
         require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./lua/snippets/" } })
       end,
     },
