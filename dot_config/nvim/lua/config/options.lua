8e61bc8c7f284cdbff1edcf4d5e287a57e4a6592
Matt Furden <zolrath@gmail.com>
2023-01-17 15:27:36 -0800

    switch vim shell to bash

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,6 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
+-- use bash for vim shell as supposedly fish is a bit slower in vims context and plugins assume bash.
+vim.opt.shell = "bash"
+
 -- disable some extension providers
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
