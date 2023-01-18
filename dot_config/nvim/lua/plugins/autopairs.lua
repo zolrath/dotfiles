00a23c6d44fd1d093cf74746ba42d82eab8bc551
Matt Furden <zolrath@gmail.com>
2023-01-17 22:04:00 -0800

    switch from mini.pairs to nvim-autopairs

---
+++ dot_config/nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,12 @@
+return {
+  -- auto pairs
+  {
+    "echasnovski/mini.pairs",
+    enabled = false,
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+  },
+}
