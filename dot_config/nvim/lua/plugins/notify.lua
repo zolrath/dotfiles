92df6979fd73f479443c740b6c196cd27d11cc9e
Matt Furden <zolrath@gmail.com>
2023-01-19 01:54:11 -0800

    use compact mode for nvim-notify

---
+++ dot_config/nvim/lua/plugins/notify.lua
@@ -0,0 +1,11 @@
+return {
+  -- notify customization
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      stages = "fade_in_slide_out",
+      timeout = 2000,
+      render = "compact",
+    },
+  },
+}
