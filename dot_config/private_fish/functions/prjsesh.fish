06273044e0ed90810e10b19e05f3a51854658ae9
Matt Furden <zolrath@gmail.com>
2023-01-19 15:56:23 -0800

    add layout selection to prjsesh

--- dot_config/private_fish/functions/prjsesh.fish
+++ dot_config/private_fish/functions/prjsesh.fish
@@ -1,32 +1,51 @@
 function prjsesh --description "create session in project folder"
-    if ! command -v fzf >/dev/null
-        echo >&2 "fzf command not found"
-        return 1
-    end
+  if ! command -v fzf >/dev/null
+    echo >&2 "fzf command not found"
+    return 1
+  end
 
-    # determine the project home
-    set -q PROJECTS || set PROJECTS ~/code
-    set -l gitprj_git $PROJECTS/*/.git/ $PROJECTS/*/*/.git/
-    set -l gitprj (dirname $gitprj_git)
-    set prjlist (string replace $PROJECTS/ "" (realpath $gitprj))
-    set selection (printf "%s\n" $prjlist | sort | fzf --layout=reverse-list --query="$argv")
-    set session_name (basename $selection)
+  # determine the project home
+  set -q PROJECTS || set PROJECTS ~/code
+  set -l gitprj_git $PROJECTS/*/.git/ $PROJECTS/*/*/.git/
+  set -l gitprj (dirname $gitprj_git)
+  set prjlist (string replace $PROJECTS/ "" (realpath $gitprj))
+  set selection (printf "%s\n" $prjlist | sort | fzf --layout=reverse-list --query="$argv")
 
-    set -l zj_sessions (zellij list-sessions)
+  if not test -n "$selection"
+    echo "No project selection made"
+    return 1
+  end
 
-    if test (contains $session_name $zj_sessions)
-        zellij attach "$session_name"
+  set session_name (basename $selection)
+  set -l zj_sessions (zellij list-sessions)
+
+  # If the session already exists just attach to it
+  if contains "$session_name" $zj_sessions
+    echo "Project session ($session_name) already exists, attaching."
+    zellij attach "$session_name"
+  else
+    # Move to the project folder
+    if test -d $PROJECTS/$selection
+      cd $PROJECTS/$selection
+    else if test -d $selection
+      cd "$selection"
     else
-      if test -d $PROJECTS/$selection
-          cd $PROJECTS/$selection
-          zellij attach -c "$session_name"
-          prevd
-      else if test -d $selection
-          cd "$selection"
-          zellij attach -c "$session_name"
-          prevd
-      else
-          echo $PROJECTS/$selection
-      end
+      echo "Project folder not found."
+      echo $PROJECTS/$selection
+      retun 1
     end
+
+    # Select a layout
+    set -l layout (_zellij_pick_layout)
+
+    # If they selected a layout, use it, otherwise just make the session
+    if test -n "$layout"
+      echo "Creating new session $session_name with layout $layout"
+      zellij -s "$session_name" -l "$layout"
+    else
+      echo "Creating new session $session_name"
+      zellij -s "$session_name"
+    end
+    prevd
+  end
 end
