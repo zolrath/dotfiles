4bfa0ae01b735070e5ee0b60dc98d1346d59e679
Matt Furden <zolrath@gmail.com>
2023-01-20 02:45:25 -0800

    add cg* keymap to vim

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -7,6 +7,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "cg*", "*Ncgn")
 
 vim.keymap.set("n", ";", ":")
 
