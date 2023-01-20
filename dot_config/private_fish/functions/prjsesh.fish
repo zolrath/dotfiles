f802972028acf1df16653169fbd8c5db4a4ee896
Matt Furden <zolrath@gmail.com>
2023-01-19 17:21:11 -0800

    use ! instead of not in fish function

--- dot_config/private_fish/functions/prjsesh.fish
+++ dot_config/private_fish/functions/prjsesh.fish
@@ -8,10 +8,11 @@ function prjsesh --description "create session in project folder"
   set -q PROJECTS || set PROJECTS ~/code
   set -l gitprj_git $PROJECTS/*/.git/ $PROJECTS/*/*/.git/
   set -l gitprj (dirname $gitprj_git)
+
   set prjlist (string replace $PROJECTS/ "" (realpath $gitprj))
   set selection (printf "%s\n" $prjlist | sort | fzf --layout=reverse-list --query="$argv")
 
-  if not test -n "$selection"
+  if ! test -n "$selection"
     echo "No project selection made"
     return 1
   end
