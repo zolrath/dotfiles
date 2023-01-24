1e071f9cebebeb466115121dc107ea880b78d10c
Matt Furden <zolrath@gmail.com>
2023-01-24 14:14:28 -0800

    add mini-animate to nvim

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
   },
   defaults = {
     lazy = true, -- every plugin is lazy-loaded by default
