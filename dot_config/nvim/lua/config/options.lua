177969f4cfa30b92948f9d4adb66dab3c7c13035
Matt Furden <zolrath@gmail.com>
2023-01-30 15:04:18 -0800

    ensure win32yank.exe exists for WSL

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -9,30 +9,3 @@ vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
-
-vim.g.colorscheme = "catppuccin-macchiato"
-vim.opt.guifont = "JetBrainsMono Nerd Font:h11"
-
--- Set paste directly - greatly improves startup time
-local is_wsl = os.getenv("WSL_DISTRO_NAME") ~= nil
-local is_osx = vim.loop.os_uname().sysname == "Darwin"
-
-if is_wsl then
-  vim.g.clipboard = {
-    copy = { ["+"] = { "clip.exe" }, ["*"] = { "clip.exe" } },
-    paste = { ["+"] = { "nvim_paste" }, ["*"] = { "nvim_paste" } },
-    cache_enabled = true,
-  }
-elseif is_osx then
-  vim.g.clipboard = {
-    copy = { ["+"] = "pbcopy", ["*"] = "pbcopy" },
-    paste = { ["+"] = "pbpaste", ["*"] = "pbpaste" },
-    cache_enabled = true,
-  }
-else
-  vim.g.clipboard = {
-    copy = { ["+"] = "xsel --nodetach -i -b", ["*"] = "xsel --nodetach -i -p" },
-    paste = { ["+"] = "xsel  -o -b", ["*"] = "xsel  -o -b" },
-    cache_enabled = true,
-  }
-end
