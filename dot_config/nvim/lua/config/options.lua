9ef55356ec875bde8811ae0da6a6ca17b361ae31
Matt Furden <zolrath@gmail.com>
2023-02-05 14:11:33 -0800

    reinsert clipboard bindings for vim

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -9,3 +9,32 @@ vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
+-- Set paste directly - greatly improves startup time
+local is_wsl = vim.fn.has("wsl")
+local is_osx = vim.loop.os_uname().sysname == "Darwin"
+
+if is_wsl then
+  vim.g.clipboard = {
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = true,
+  }
+elseif is_osx then
+  vim.g.clipboard = {
+    copy = { ["+"] = "pbcopy", ["*"] = "pbcopy" },
+    paste = { ["+"] = "pbpaste", ["*"] = "pbpaste" },
+    cache_enabled = true,
+  }
+else
+  vim.g.clipboard = {
+    copy = { ["+"] = "xsel --nodetach -i -b", ["*"] = "xsel --nodetach -i -p" },
+    paste = { ["+"] = "xsel  -o -b", ["*"] = "xsel  -o -b" },
+    cache_enabled = true,
+  }
+end
