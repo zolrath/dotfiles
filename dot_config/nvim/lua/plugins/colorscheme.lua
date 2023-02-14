f6231050fa6c25794edf0c27b229936de3d23986
Matt Furden <zolrath@gmail.com>
2023-02-13 17:43:06 -0800

    add catppuccin integrations in vim

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -5,11 +5,25 @@ return {
   },
   {
     "catppuccin/nvim",
+    name = "catppuccin",
     config = function()
       local catppuccin = require("catppuccin")
       catppuccin.setup({
         flavour = "macchiato",
         transparent_background = false,
+
+        integrations = {
+          which_key = true,
+          cmp = true,
+          telescope = true,
+          mini = true,
+          leap = true,
+          noice = true,
+          dap = true,
+          illuminate = true,
+          ts_rainbow = true,
+          neotree = true,
+        },
       })
       catppuccin.load()
     end,
