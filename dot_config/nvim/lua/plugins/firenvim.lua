3071e24fbe3a0ac87b9e62765357ee45cd26224d
Matt Furden <zolrath@gmail.com>
2023-02-21 14:52:32 -0800

    update firenvim build command

--- dot_config/nvim/lua/plugins/firenvim.lua
+++ dot_config/nvim/lua/plugins/firenvim.lua
@@ -26,10 +26,11 @@ return {
   },
   {
     "glacambre/firenvim",
+    cond = not not vim.g.started_by_firenvim,
     build = function()
+      require("lazy").load({ plugins = "firenvim", wait = true })
       vim.fn["firenvim#install"](0)
     end,
-    cond = not not vim.g.started_by_firenvim,
     config = function()
       local elixirSites = { ".*exercism\\.org/tracks/elixir/.*" }
       local rustSites = { ".*exercism\\.org/tracks/rust/.*", ".*leetcode.com/*" }
