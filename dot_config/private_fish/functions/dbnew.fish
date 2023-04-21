aa4a42fc8f1c89cdcf7a6d070d1c07f9f15348f3
Matt Furden <zolrath@gmail.com>
2023-04-20 18:40:51 -0700

    protect against including _dev in dbnew

--- dot_config/private_fish/functions/dbnew.fish
+++ dot_config/private_fish/functions/dbnew.fish
@@ -1,9 +1,18 @@
-function dbnew
-    dbdown
-    docker run --name $argv[1]_dev -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres 1>/dev/null
-    if test $status -eq 0
-        echo "Created new dev db: $argv[1]_dev"
+function dbnew --argument-names db_name
+    if test -n "$db_name"
+        dbdown
+        set filtered (string replace -r '_dev$' '' $db_name)_dev
+
+        docker run --name $filtered -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres 1>/dev/null
+        if test $status -eq 0
+            echo "Created new dev db: $filtered"
+        else
+            echo "Failed to new dev db: $filtered"
+        end
     else
-        echo "Failed to new dev db: $argv[1]_dev"
+        echo "You must provide a database name: `dbnew <dbname>`"
+        echo "REMINDER: This command will append _dev to the name provided."
+        echo "Simply use the name of the project, do not include _dev yourself."
     end
+
 end
--- dot_config/private_fish/functions/dbup.fish
+++ dot_config/private_fish/functions/dbup.fish
@@ -3,9 +3,7 @@ function dbup
         if string match -q "$argv[1]_dev" in (docker ps --format '{{.Names}}')
             echo "$argv[1]_dev already running"
         else
-            echo "Stopping all other containers using port 5432."
             dbdown
-
             echo Starting (docker start $argv[1]_dev)
         end
     else
