4373525b01dc369412c9a24ed3485083cc865506
Matt Furden <zolrath@gmail.com>
2023-04-20 18:13:01 -0700

    Improve db commands

--- dot_config/private_fish/functions/dblist.fish
+++ dot_config/private_fish/functions/dblist.fish
@@ -2,7 +2,13 @@ function dblist
     # --filter "expose=5432" 
     # seems stopped containers don't report their previously used ports
     # so this filters too aggressively
-    for db in (docker ps --all --filter "name=_dev\$" --format "{{.Names}}")
-        echo (string replace -r '_dev$' '' $db)
+    #
+    set active (docker ps --format '{{.Names}}' --filter "expose=5432" --filter "name=_dev\$")
+    for db in (docker ps --all --filter "name=_dev\$" --format "{{.Names}}" | sort)
+        if string match -q $active $db
+            echo "👉" (string replace -r '_dev$' '' $db)
+        else
+            echo "  " (string replace -r '_dev$' '' $db)
+        end
     end
 end
--- dot_config/private_fish/functions/dbnew.fish
+++ dot_config/private_fish/functions/dbnew.fish
@@ -1,3 +1,4 @@
 function dbnew
+    dbdown
     docker run --name $argv[1]_dev -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres
 end
