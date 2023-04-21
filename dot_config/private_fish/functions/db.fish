9ce5c1df5cff1da6fa931642d5b6865ac4dc20b4
Matt Furden <zolrath@gmail.com>
2023-04-20 19:42:22 -0700

    split db commands into new lines for readability

--- dot_config/private_fish/functions/db.fish
+++ dot_config/private_fish/functions/db.fish
@@ -1,5 +1,10 @@
 function db
-    set dbname (docker ps --all --filter "name=_dev\$" --format "{{.Names}}" | string replace -r '_dev$' '' | sort | fzf --select-1 --query="$argv" --prompt="start db> ")
+    set dbname (
+          docker ps --all --filter "name=_dev\$" --format "{{.Names}}" | 
+          string replace -r '_dev$' '' |
+          sort | 
+          fzf --select-1 --query="$argv" --prompt="start db> "
+    )
     if not string length -q -- $dbname
         echo "No database selected."
         echo "To create a new dev database use `dbnew <dbname>`"
