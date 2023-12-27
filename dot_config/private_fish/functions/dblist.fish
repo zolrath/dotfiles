f0686bd461a970555a5928f7e3e8373b0c82ad32
Matt Furden <zolrath@gmail.com>
2023-12-27 12:52:28 -0800

    remove port filter on dblist

--- dot_config/private_fish/functions/dblist.fish
+++ dot_config/private_fish/functions/dblist.fish
@@ -1,8 +1,4 @@
 function dblist
-    # --filter "expose=5432" 
-    # seems stopped containers don't report their previously used ports
-    # so this filters too aggressively
-    #
     set active (docker ps --format '{{.Names}}' --filter "expose=5432" --filter "name=_dev\$")
     for db in (docker ps --all --filter "name=_dev\$" --format "{{.Names}}" | sort)
         if string match -q $active $db
