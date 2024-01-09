0e777ce3ccb5d1d19bbbb790c79551a247819069
Matt Furden <zolrath@gmail.com>
2024-01-08 18:35:37 -0800

    use unnamedplus register for neoclip

--- dot_config/nvim/lua/plugins/neoclip.lua
+++ dot_config/nvim/lua/plugins/neoclip.lua
@@ -2,11 +2,14 @@ return {
   {
     "AckslD/nvim-neoclip.lua",
     event = "TextYankPost",
+    dependencies = { "kkharji/sqlite.lua", module = "sqlite" },
     keys = {
       { "<C-y>", "<cmd>Telescope neoclip<cr>", desc = "Neoclip Telescope" },
     },
     config = function()
       require("neoclip").setup({
+        enable_peristent_history = true,
+        default_register = "+",
         on_select = {
           move_to_front = true,
         },
