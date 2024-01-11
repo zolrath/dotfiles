2d39dfa040231439a23f70d9d2b8fb845c4a1647
Matt Furden <zolrath@gmail.com>
2024-01-11 13:00:20 -0800

    add nvim-scissors for snippet search/edit/add

--- dot_config/nvim/lua/plugins/snippets.lua
+++ dot_config/nvim/lua/plugins/snippets.lua
@@ -5,4 +5,30 @@ return {
       require("luasnip.loaders.from_vscode").load({ paths = "~/.config/nvim/snippets" })
     end,
   },
+  {
+    "chrisgrieser/nvim-scissors",
+    dependencies = "nvim-telescope/telescope.nvim",
+    opts = {
+      snippetDir = "~/.config/nvim/snippets/",
+      telescope = { alsoSearchSnippetBody = true },
+      jsonFormatter = "jq",
+    },
+    keys = {
+      {
+        "<Leader>he",
+        function()
+          require("scissors").editSnippet()
+        end,
+        desc = "Edit Snippet",
+      },
+      {
+        "<Leader>ha",
+        function()
+          require("scissors").addNewSnippet()
+        end,
+        mode = { "n", "x" },
+        desc = "Add new Snippet",
+      },
+    },
+  },
 }
