204f1549dc0753266ba1f43b98d2d04539e0467a
Matt Furden <zolrath@gmail.com>
2024-01-23 21:06:02 -0800

    Edit file directly in editfish/editvim

--- dot_config/private_fish/functions/editfish.fish
+++ dot_config/private_fish/functions/editfish.fish
@@ -1,7 +1,7 @@
-function editfish 
-  cd (chezmoi source-path)/dot_config/private_fish/
-  chezmoi edit ~/.config/fish/config.fish
-  chezmoi apply
-  source ~/.config/fish/config.fish
-  prevd
+function editfish
+    cd (chezmoi source-path)/dot_config/private_fish/
+    vim config.fish
+    chezmoi apply
+    source ~/.config/fish/config.fish
+    prevd
 end
--- dot_config/private_fish/functions/editvim.fish
+++ dot_config/private_fish/functions/editvim.fish
@@ -1,7 +1,7 @@
-function editvim 
-  cd (chezmoi source-path)/dot_config/nvim/
-  chezmoi edit ~/.config/nvim/init.lua
-  chezmoi add ~/.config/nvim/lazy-lock.json
-  chezmoi apply
-  prevd
+function editvim
+    cd (chezmoi source-path)/dot_config/nvim/
+    vim init.lua
+    chezmoi add ~/.config/nvim/lazy-lock.json
+    chezmoi apply
+    prevd
 end
