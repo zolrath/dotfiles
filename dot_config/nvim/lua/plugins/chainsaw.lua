0ed4fa9c7292729f267e89b70f77b927889c31fd
Matt Furden <zolrath@gmail.com>
2025-01-06 00:49:24 -0800

    add chainsaw for logging - custom fork for now

---
+++ dot_config/nvim/lua/plugins/chainsaw.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    --"chrisgrieser/nvim-chainsaw",
+    "zolrath/nvim-chainsaw",
+    branch = "python-logs",
+    event = "VeryLazy",
+    opts = {
+      marker = "🪵",
+    },
+    keys = {
+      { "<leader>l", "", desc = "+log", mode = { "n", "v" } },
+      {
+        "<leader>ll",
+        function()
+          require("chainsaw").variableLog()
+        end,
+        mode = { "n", "v" },
+        desc = "Log variable",
+      },
+      {
+        "<leader>lo",
+        function()
+          require("chainsaw").objectLog()
+        end,
+        mode = { "n", "v" },
+        desc = "Log object",
+      },
+      {
+        "<leader>lt",
+        function()
+          require("chainsaw").timeLog()
+        end,
+        mode = { "n", "v" },
+        desc = "Log time",
+      },
+      {
+        "<leader>lc",
+        function()
+          require("chainsaw").removeLogs()
+        end,
+        mode = { "n", "v" },
+        desc = "Clear logs",
+      },
+    },
+  },
+}
